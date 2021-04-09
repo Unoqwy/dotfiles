3231f757f7c33ca7641195dc5f9d8d47a8e6d7cd
Unoqwy <julien.qwy@gmail.com>
2021-04-09 21:36:27 +0200

    rust full support!

--- .config/nvim/lua/editor/langs/rust.lua
+++ .config/nvim/lua/editor/langs/rust.lua
@@ -1,13 +1,21 @@
 local M = {}
 
 function M.setup_lsp()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = { properties = { 'additionalTextEdits' } }
     require('lspconfig').rust.setup({
+        capabilities = capabilities,
         on_attach = q.lsp.on_attach,
         settings = {
             ["rust-analyzer"] = {
                 assist = {
                     importMergeBehavior = "last",
-                    importPrefix = "by_self",
+                    importPrefix = "by_crate",
+                },
+
+                cargo = {
+                    loadOutDirsFromCheck = true, -- eq. runBuildScripts
                 },
                 procMacro = {
                     enable = true,
