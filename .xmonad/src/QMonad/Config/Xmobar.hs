ed24838f1c2d2771d273cb838d5eaee7fff9dcbf
Unoqwy <julien.qwy@gmail.com>
2021-07-30 17:22:41 +0200

    treat ws with all wins minimized as empty

--- .xmonad/src/QMonad/Config/Xmobar.hs
+++ .xmonad/src/QMonad/Config/Xmobar.hs
@@ -16,6 +16,13 @@ import XMonad.Hooks.DynamicLog (
         wrap, shorten, xmobarStripTags,
     )
 
+import XMonad.Util.Minimize (Minimized(minimizedStack))
+import qualified XMonad.Util.ExtensibleState as XS
+import System.IO (Handle)
+import qualified XMonad.StackSet as W
+
+import Data.List (filter)
+
 -- Imports/ Shared
 import qualified QMonad.Shared.Theme as T
 
@@ -28,7 +35,10 @@ cwrap wks a b c = wrap a b (wrap ("<action=`xdotool key super+" ++ wks ++ "`><fn
 -- Goal of this log hook:
 --   Send the workspaces to xmobar and read it there using the unsafe stdin reader
 --   Bind the title + layout to a FIFO so it can be severed from workspaces
-wsLogHook xmobarStdin = workspaceMasksPP (def {
+--
+-- If all windows in a hidden workspace are minimized, display it as a hiddenNoWindows ws.
+wsLogHook :: Handle -> (WorkspaceId -> Bool) -> X()
+wsLogHook xmobarStdin lookup = workspaceMasksPP (def {
       ppOutput  = hPutStrLn xmobarStdin
     , ppOrder   = \(ws:_) -> [ws]
     , ppWsSep   = ""
@@ -37,19 +47,33 @@ wsLogHook xmobarStdin = workspaceMasksPP (def {
         then cwrap w ("<box type=Top width=1 offset=C10 color=" ++ T.primaryColor ++ "><fc=" ++ T.primaryColor ++ ">") "</fc></box>" n
         else cwrap w ("<box type=Top width=1 offset=C10 color=" ++ T.secondColor ++ "><fc=" ++ T.secondColor ++ ">") "</fc></box>" n
     , wsppHidden  = \(WorkspaceMask w visible n) _ -> if visible
-        then cwrap w "" "" n else ""
+        then if lookup w then cwrap w "" "" n else cwrap w "<fc=#49464e>" "</fc>" n
+        else ""
     , wsppHiddenNoWindows  = \(WorkspaceMask w visible n) _ -> if visible
         then cwrap w "<fc=#49464e>" "</fc>" n else ""
   }) >>= dynamicLogWithPP
 
-infoLogHook infoPipe = dynamicLogWithPP def {
+infoLogHook :: Handle -> Bool -> X()
+infoLogHook infoPipe showTitle = do
+  dynamicLogWithPP def {
       ppOutput = hPutStrLn infoPipe
     , ppOrder  = \(_:l:t:_) -> [l,t]
 
     -- drop 9 strips "Minimize " prefix from layout
     , ppLayout = wrap ("<box type=Bottom width=2 color=" ++ T.secondColor ++ "><fc=" ++ T.secondColor ++ ">") "</fc></box>" . drop 9
-    , ppTitle  = wrap ("<fc=" ++ T.darkFgColor ++ ">") "</fc>" . xmobarStrip . shorten 40
-    , ppSep    = " "
+    , ppTitle = \title -> if showTitle then shorten 40 . xmobarStrip $ wrap ("<fc=" ++ T.darkFgColor ++ ">") "</fc>" title else ""
+    , ppSep  = " "
   }
 
-xmobarLogHook xmobarStdin infoPipe = wsLogHook xmobarStdin <+> infoLogHook infoPipe
+xmobarLogHook :: Handle -> Handle -> X()
+xmobarLogHook xmobarStdin infoPipe = do
+  minimized <- XS.gets minimizedStack
+  wset <- gets windowset
+
+  let allMinimizedWS = map W.tag $
+        filter (\w -> not $ any (`notElem` minimized) (W.integrate' $ W.stack w)) (W.workspaces wset)
+  wsLogHook xmobarStdin (`notElem` allMinimizedWS)
+
+  -- don't show title in status bar if all windows are minimized
+  let currentWS = W.workspace $ W.current wset
+  infoLogHook infoPipe (any (`notElem` minimized) (W.integrate' $ W.stack currentWS))
